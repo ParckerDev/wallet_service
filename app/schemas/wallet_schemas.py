f018916e50a5fd7d7a9370184206db681bfa1925
Alexandr Shevchenko <djpestro@gmail.com>
2025-01-23 19:35:51 +0300

    add docstring in wallet_schemas

--- app/schemas/wallet_schemas.py
+++ app/schemas/wallet_schemas.py
@@ -4,15 +4,39 @@ from enum import Enum
 
 
 class OperationType(str, Enum):
+    """
+    Enum для определения типа операции с кошельком.
+
+    Доступные типы операций:
+    - DEPOSIT: Пополнение кошелька.
+    - WITHDRAW: Снятие средств с кошелька.
+    """
+
     DEPOSIT = "DEPOSIT"
     WITHDRAW = "WITHDRAW"
 
 
 class Operation(BaseModel):
+    """
+    Модель для представления операции с кошельком.
+
+    Attributes:
+        operation_type (OperationType): Тип операции (пополнение или снятие).
+        amount (Decimal): Сумма операции.
+    """
+
     operation_type: OperationType
     amount: Decimal
 
 
 class WalletResponse(BaseModel):
+    """
+    Модель для представления ответа о состоянии кошелька.
+
+    Attributes:
+        uuid (str): Уникальный идентификатор кошелька.
+        balance (float): Текущий баланс кошелька.
+    """
+
     uuid: str
     balance: float
