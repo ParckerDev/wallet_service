a74c83ad8884eccfb359dac05ec8024b0d1b7084
Alexandr Shevchenko <djpestro@gmail.com>
2025-01-24 12:14:23 +0600

    update test logic

--- tests/test.py
+++ tests/test.py
@@ -1,16 +1,18 @@
 from locust import HttpUser, task, between
 import random
 
+
 class WalletUser(HttpUser):
-    wait_time = between(1, 2)  # Время ожидания между запросами
-    wallet_uuid = "fa799d59-d053-4f33-84f8-042be5815c4b"
-    
+    wait_time = between(1, 5)  # Время ожидания между запросами
+    wallet_uuid = "7be560f5-0c21-439d-9a57-e62c51ce5f93"
 
     def create_wallet(self):
         """
         Отправляет GET-запрос на создание нового кошелька и сохраняет его UUID.
         """
-        response = self.client.get("/api/v1/wallets/new")  # Запрос на создание нового кошелька
+        response = self.client.get(
+            "/api/v1/wallets/new"
+        )  # Запрос на создание нового кошелька
         if response.status_code == 201:
             # Предполагаем, что сервер возвращает JSON с UUID кошелька
             wallet_data = response.json()  # Извлекаем JSON-ответ
@@ -25,23 +27,22 @@ class WalletUser(HttpUser):
         """
         Выполняет операцию депозита на созданный кошелек.
         """
-        amount = random.uniform(1.0, 100.0)  # Генерируем случайную сумму для депозита
-        self.client.post(f"/api/v1/wallets/{self.wallet_uuid}/operation", json={
-            "operation_type": "DEPOSIT",
-            "amount": amount
-        })
+        amount = random.randint(1, 100)  # Генерируем случайную сумму для депозита
+        self.client.post(
+            f"/api/v1/wallets/{self.wallet_uuid}/operation",
+            json={"operation_type": "DEPOSIT", "amount": amount},
+        )
 
     @task
     def withdraw(self):
         """
         Выполняет операцию вывода с созданного кошелька.
         """
-        amount = random.uniform(1.0, 100.0)  # Генерируем случайную сумму для вывода
-        self.client.post(f"/api/v1/wallets/{self.wallet_uuid}/operation", json={
-            "operation_type": "WITHDRAW",
-            "amount": amount
-        })
-
+        amount = random.randint(1, 100)  # Генерируем случайную сумму для вывода
+        self.client.post(
+            f"/api/v1/wallets/{self.wallet_uuid}/operation",
+            json={"operation_type": "WITHDRAW", "amount": amount},
+        )
 
 
 # for run test input:
